1c718f9ef4ace99072e17e9705d1a2a90de2ace9
jacklucia <jackpierrelucia@gmail.com>
2018-10-04 15:28:22 -0700

    added the dankest new print statement, even danker

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,12 @@ public class Main {
     public static void  main (String[] args){
         System.out.print("Hello World");
         System.out.print("Shrekspacito 4");
+
+        int shrek = 3;
+
+        if (shrek == 5)
+        System.out.print("correct");
+        else
+            System.out.print("ur fattest gay");
     }
 }
