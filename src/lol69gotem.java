ebbeefcabd74be93d85d28cdbe1deb92cddd4bfe
commonafro <clutchman367@gmail.com>
2018-10-04 15:39:43 -0700

    Added file lol69gotem.java

---
+++ src/lol69gotem.java
@@ -0,0 +1,10 @@
+public class lol69gotem {
+    public static void  main (String[] args){
+        int powerLevel=9001;
+
+        if (powerLevel>9000){
+            System.out.print("It's over 9000!");
+        }
+
+    }
+}
