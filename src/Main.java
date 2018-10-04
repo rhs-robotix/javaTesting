d4743f6358ec26c6bb1c1019cffa1c9b25f827ad
briannapeters <brianna.peterson@student.rjuhsd.us>
2018-10-04 15:27:15 -0700

    Wrote Hello

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void  main (String[] args){
         System.out.print("Hello World");
         System.out.print("Shrekspacito 4");
+        System.out.print("The Bubbles are back!");
+
+
     }
 }
