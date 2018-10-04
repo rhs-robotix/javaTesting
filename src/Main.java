e6675ee6ad541f1c0e5acc8df198230ae8460dec
jacklucia <jackpierrelucia@gmail.com>
2018-10-04 15:51:17 -0700

    get out me swump

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,21 @@
+
+import java.util.Random;
+
 public class Main {
     public static void  main (String[] args){
+        Random rand = new Random();
+
+
         System.out.println("Hello World");
         System.out.println("Shrekspacito 4");
 
-        int shrek = 3;
-
-        if (shrek == 5)
-        System.out.println("correct");
+        int shrek = rand.nextInt(10);
+    while (true) {
+        shrek = rand.nextInt(10);
+        if (shrek <= 5)
+            System.out.println("correct");
         else
             System.out.println("ur fattest gay");
+    }
     }
 }
