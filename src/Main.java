cf31e9472185ca8fb1c3faa33519df0e58b8e10a
commonafro <clutchman367@gmail.com>
2018-10-04 15:27:52 -0700

    Added print statement shrekspacito sequels

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void  main (String[] args){
         System.out.print("Hello World");
         System.out.print("Shrekspacito 4");
+        System.out.print("Shrekspacito 5");
+        System.out.print("Shrekspacito 6");
+        System.out.print("Shrekspacito 7");
+        System.out.print("Shrekspacito 69 lol gotem");
     }
 }
